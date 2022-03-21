0dc40aa224608190e2394768f6845fa360d660a3
chenjiaxv <1587198543@qq.com>
2022-03-21 23:31:52 +0800

    实现analysis接口

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
-from io import BytesIO
+
+from service.AnalysisService import analysis
 
 app = Flask(__name__)
 
@@ -8,12 +9,16 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+
 @app.route('/analysis', methods=['post'])
 def imgAnalysis():
     srcFileName = request.form.get('source_image')
     path = "F:/demo/res/img/"
     srcFilePath = path + srcFileName
+    fileName, yang, ying = analysis(srcFilePath)
+    tps = round(yang / (yang + ying) * 100, 1)
+    return jsonify([fileName, yang, ying, tps])
 
 
 if __name__ == '__main__':
-    app.run(debug=False, port = 8442)
+    app.run()
--- service/AnalysisService.py
+++ service/AnalysisService.py
@@ -1,9 +1,18 @@
-import torch
-from model import UNet
-import utils
-import torchvision.transforms as T
+import uuid
 
-localizationWeights = r'../checkpoints/MBM/epoch_10.pth.tar'
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import torch
+import torchvision.transforms as T
+from keras.applications.xception import preprocess_input
+from tensorflow import keras
+
+from service import utils
+from service.model import UNet
+
+localizationWeights = r'F:\demo\flask_backend\checkpoints\MBM\epoch_10.pth.tar'
+classifyModel = r'F:\demo\flask_backend\checkpoints\LUSC\my_model.h5'
 
 
 def detect(srcImgPath):
@@ -19,3 +28,71 @@ def detect(srcImgPath):
         x = x.to(utils.device())
         pred = utils.ensure_array(model(x)).squeeze(0)
         points = utils.extract_points_from_direction_field_map(pred, lambda1=0.7, step=10)
+
+    # save image and points
+    image_array = np.array(image)
+    plt.figure(dpi=500)
+    plt.imshow(image_array)
+    plt.axis('off')
+    points_array = np.array(points)
+    plt.plot(points_array[:, 1], points_array[:, 0], marker='o', markerfacecolor='#f9f738', markeredgecolor='none',
+             markersize=2,
+             linestyle='none')
+    uid = str(uuid.uuid4())
+    fileName = ''.join(uid.split('-')) + ".png"
+    savePath = 'F:/demo/res/img/' + fileName
+    plt.savefig(savePath, bbox_inches='tight')
+    plt.close()
+
+    return fileName, image, points
+
+
+def getPatch(image, point):
+    xMax, yMax = image.size
+    x, y = point
+    l = (x - 30) if ((x - 30) > 0) else 0
+    r = (x + 30) if ((x + 30) < xMax) else xMax
+    h = (y - 30) if ((y - 30) > 0) else 0
+    b = (y + 30) if ((y + 30) < yMax) else yMax
+    box = (l, h, r, b)
+
+    patch = image.crop(box)
+    return patch
+
+
+def classify(model, image):
+    x = np.array(image)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+
+    predict = model.predict(x)
+    # print(predict)
+
+    if round(predict[0, 0]) == 0:
+        result = 'ying'
+    else:
+        result = 'yang'
+
+    return result
+
+
+def analysis(srcImgPath):
+    # 获取细胞位置
+    fileName, image, points = detect(srcImgPath)
+    # print(len(points))
+
+    model = keras.models.load_model(classifyModel)
+    yang = 0
+    ying = 0
+    for point in points:
+        patch = getPatch(image, point)
+        patch = tf.image.resize(patch, [80, 80])
+        patch = tf.cast(patch, tf.float32)
+        patch = patch / 255
+
+        result = classify(model, patch)
+        if result == 'yang':
+            yang += 1
+        else:
+            ying += 1
+    return fileName, yang, ying
